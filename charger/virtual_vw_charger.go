af5ee38f804555f6225e0fa92d3a771b5df1b87f
waldtraut1981 <73784107+waldtraut1981@users.noreply.github.com>
2022-02-27 08:47:37 +0100

    added position check for vehicle

--- charger/virtual_vw_charger.go
+++ charger/virtual_vw_charger.go
@@ -2,6 +2,7 @@ package charger
 
 import (
 	"fmt"
+	"math"
 	"strings"
 	"time"
 
@@ -31,6 +32,7 @@ type VwVirtualCharger struct {
 	ChargeTransition
 	*util.Logger
 	callCounterDuringTransistion int
+	Latitude, Longitude          float64
 }
 
 func init() {
@@ -42,6 +44,7 @@ func NewVirtualVwFromConfig(other map[string]interface{}) (api.Charger, error) {
 	fmt.Println("NewVirtualVwFromConfig")
 	cc := struct {
 		User, Password, VIN string
+		Latitude, Longitude float64
 		Cache               time.Duration
 		Timeout             time.Duration
 	}{
@@ -100,16 +103,18 @@ func NewVirtualVwFromConfig(other map[string]interface{}) (api.Charger, error) {
 		log.DEBUG.Println("Reusing provider for VIN.")
 	}
 
-	return NewVwVirtual(provider, log)
+	return NewVwVirtual(provider, log, cc.Latitude, cc.Longitude)
 }
 
 // NewVwVirtual creates VW virtual charger
-func NewVwVirtual(provider *vw.Provider, log *util.Logger) (*VwVirtualCharger, error) {
+func NewVwVirtual(provider *vw.Provider, log *util.Logger, latitude float64, longitude float64) (*VwVirtualCharger, error) {
 	c := &VwVirtualCharger{
 		Provider:                     provider,
 		ChargeTransition:             StateReached,
 		Logger:                       log,
 		callCounterDuringTransistion: 0,
+		Latitude:                     latitude,
+		Longitude:                    longitude,
 	}
 
 	return c, nil
@@ -188,6 +193,22 @@ func (c *VwVirtualCharger) MaxCurrent(current int64) error {
 // Status implements the api.Charger interface
 func (c *VwVirtualCharger) Status() (api.ChargeStatus, error) {
 	//TODO: check car position before state --> is car at home near charger?
+
+	latitude, longitude, err := c.Provider.Position()
+
+	if err == nil {
+		c.Logger.ERROR.Println("Current vehicle position: lat ", latitude, " long ", longitude, " charger location: lat ", c.Latitude, " long ", c.Longitude)
+
+		distance := distance(c.Latitude, c.Longitude, latitude, longitude, "K")
+		c.Logger.ERROR.Println("Distance from vehicle to charger is (km): ", distance)
+
+		if distance > 2.0 {
+			c.Logger.ERROR.Println("vehicle is not considred to be home. Setting to fixed status StatusA (not connected)")
+			return api.StatusA, nil
+		}
+	} else {
+		c.Logger.ERROR.Println("Error getting vehicle position: ", err)
+	}
 
 	chargeStatus, err := c.Provider.Status()
 
@@ -274,4 +295,62 @@ func ensureVehicle(vin string, fun func() ([]string, error)) (string, error) {
 	}
 
 	return vin, err
+}
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//:::                                                                         :::
+//:::  This routine calculates the distance between two points (given the     :::
+//:::  latitude/longitude of those points). It is being used to calculate     :::
+//:::  the distance between two locations using GeoDataSource (TM) products  :::
+//:::                                                                         :::
+//:::  Definitions:                                                           :::
+//:::    South latitudes are negative, east longitudes are positive           :::
+//:::                                                                         :::
+//:::  Passed to function:                                                    :::
+//:::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :::
+//:::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :::
+//:::    unit = the unit you desire for results                               :::
+//:::           where: 'M' is statute miles (default)                         :::
+//:::                  'K' is kilometers                                      :::
+//:::                  'N' is nautical miles                                  :::
+//:::                                                                         :::
+//:::  Worldwide cities and other features databases with latitude longitude  :::
+//:::  are available at https://www.geodatasource.com                         :::
+//:::                                                                         :::
+//:::  For enquiries, please contact sales@geodatasource.com                  :::
+//:::                                                                         :::
+//:::  Official Web site: https://www.geodatasource.com                       :::
+//:::                                                                         :::
+//:::               GeoDataSource.com (C) All Rights Reserved 2022            :::
+//:::                                                                         :::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+func distance(lat1 float64, lng1 float64, lat2 float64, lng2 float64, unit ...string) float64 {
+	const PI float64 = 3.141592653589793
+
+	radlat1 := float64(PI * lat1 / 180)
+	radlat2 := float64(PI * lat2 / 180)
+
+	theta := float64(lng1 - lng2)
+	radtheta := float64(PI * theta / 180)
+
+	dist := math.Sin(radlat1)*math.Sin(radlat2) + math.Cos(radlat1)*math.Cos(radlat2)*math.Cos(radtheta)
+
+	if dist > 1 {
+		dist = 1
+	}
+
+	dist = math.Acos(dist)
+	dist = dist * 180 / PI
+	dist = dist * 60 * 1.1515
+
+	if len(unit) > 0 {
+		if unit[0] == "K" {
+			dist = dist * 1.609344
+		} else if unit[0] == "N" {
+			dist = dist * 0.8684
+		}
+	}
+
+	return dist
 }
