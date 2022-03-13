f2ca753b67ba345eaa3f62eccb3d92552a40813e
waldtraut1981 <73784107+waldtraut1981@users.noreply.github.com>
2022-03-13 17:22:25 +0100

    modified logging and read loop

--- meter/bosch.go
+++ meter/bosch.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http/cookiejar"
 	"net/url"
 	"strconv"
@@ -28,6 +27,7 @@ type BoschApiClient struct {
 	pvLeistungWatt         float64
 	batterieLadeStrom      float64
 	verbrauchVonBatterie   float64
+	logger                 *util.Logger
 }
 
 type Bosch struct {
@@ -35,6 +35,7 @@ type Bosch struct {
 	currentErr              error
 	currentTotalEnergyValue float64
 	requestClient           *BoschApiClient
+	logger                  *util.Logger
 }
 
 var boschInstance *BoschApiClient = nil
@@ -81,6 +82,7 @@ func NewBosch(uri, usage string) (api.Meter, error) {
 			pvLeistungWatt:         0.0,
 			batterieLadeStrom:      0.0,
 			verbrauchVonBatterie:   0.0,
+			logger:                 log,
 		}
 
 		// ignore the self signed certificate
@@ -100,6 +102,7 @@ func NewBosch(uri, usage string) (api.Meter, error) {
 		currentErr:              nil,
 		currentTotalEnergyValue: 0.0,
 		requestClient:           boschInstance,
+		logger:                  log,
 	}
 
 	// decorate api.MeterEnergy
@@ -122,13 +125,14 @@ func (m *BoschApiClient) Login() error {
 	resp, err := m.Client.Get(m.uri)
 
 	if err != nil {
-		log.Fatalln(err)
+		m.logger.ERROR.Println("Error during login: first GET", err)
 		return err
 	}
 
 	if resp.StatusCode >= 300 {
-		log.Fatal("Error while getting WUI SID. Response code was >=300:")
-		return errors.New("Error while getting WUI SID. Response code was >=300:")
+		errorText := "Error while getting WUI SID. Response code was >=300:"
+		m.logger.ERROR.Println(errorText)
+		return errors.New(errorText)
 	}
 
 	defer resp.Body.Close()
@@ -136,14 +140,14 @@ func (m *BoschApiClient) Login() error {
 	//We Read the response body on the line below.
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		m.logger.ERROR.Println("Error during login: read response body", err)
 		return err
 	}
 
 	err = extractWuiSidFromBody(m, string(body))
 
 	if err != nil {
-		log.Fatal(err)
+		m.logger.ERROR.Println("Error during login: error extract WUI SID", err)
 		return err
 	}
 
@@ -155,7 +159,8 @@ func readLoop(m *BoschApiClient) {
 		loopError := executeRead(m)
 
 		if loopError != nil {
-			break
+			m.logger.ERROR.Println("error during read loop. Try to re-login/get new WUI SID")
+			m.Login()
 		}
 
 		time.Sleep(5000 * time.Millisecond)
@@ -167,7 +172,7 @@ func executeRead(m *BoschApiClient) error {
 	resp, err := m.Client.Post(m.uri+"/cgi-bin/ipcclient.fcgi?"+m.wuSid, "text/plain", bytes.NewBuffer(postMessge))
 
 	if err != nil {
-		log.Fatal("error posting data retrieval request")
+		m.logger.ERROR.Println("Error during data retrieval request: POST", err)
 		return err
 	}
 
@@ -175,14 +180,16 @@ func executeRead(m *BoschApiClient) error {
 
 	//Read the response body
 	body, err := ioutil.ReadAll(resp.Body)
+
 	if err != nil {
-		log.Fatalln(err)
+		m.logger.ERROR.Println("Error during data retrieval request: read body", err)
 		return err
 	}
 
 	if resp.StatusCode >= 300 {
-		log.Fatal("Error while reading values. Response code was >=300:")
-		return errors.New("Error while reading values. Response code was >=300")
+		errorText := "error while reading values. response code was >=300:"
+		m.logger.ERROR.Println(errorText)
+		return errors.New(errorText)
 	}
 
 	sb := string(body)
@@ -203,6 +210,7 @@ func parseWattValue(inputString string) (float64, error) {
 
 func extractValues(m *BoschApiClient, body string) error {
 	if strings.Contains(body, "session invalid") {
+		m.logger.DEBUG.Println("extractValues: Session invalid. Performing Re-login")
 		m.Login()
 		return nil
 	}
@@ -212,6 +220,7 @@ func extractValues(m *BoschApiClient, body string) error {
 	soc, err := strconv.Atoi(values[3])
 
 	if err != nil {
+		m.logger.ERROR.Println("extractValues: error during value parsing 1", err)
 		return err
 	}
 
@@ -219,28 +228,34 @@ func extractValues(m *BoschApiClient, body string) error {
 	m.einspeisung, err = parseWattValue(values[11])
 
 	if err != nil {
+		m.logger.ERROR.Println("extractValues: error during value parsing 2", err)
 		return err
 	}
 
 	m.strombezugAusNetz, err = parseWattValue(values[14])
 
 	if err != nil {
+		m.logger.ERROR.Println("extractValues: error during value parsing 3", err)
 		return err
 	}
 
 	m.pvLeistungWatt, err = parseWattValue(values[2])
 
 	if err != nil {
+		m.logger.ERROR.Println("extractValues: error during value parsing 4", err)
 		return err
 	}
 
 	m.batterieLadeStrom, err = parseWattValue(values[10])
 
 	if err != nil {
+		m.logger.ERROR.Println("extractValues: error during value parsing 5", err)
 		return err
 	}
 
 	m.verbrauchVonBatterie, err = parseWattValue(values[13])
+
+	m.logger.DEBUG.Println("extractValues: batterieLadeStrom=", m.batterieLadeStrom, ";currentBatterySocValue=", m.currentBatterySocValue, ";einspeisung=", m.einspeisung, ";pvLeistungWatt=", m.pvLeistungWatt, ";strombezugAusNetz=", m.strombezugAusNetz, ";verbrauchVonBatterie=", m.verbrauchVonBatterie)
 
 	return err
 }
@@ -250,10 +265,13 @@ func extractWuiSidFromBody(m *BoschApiClient, body string) error {
 
 	if index < 0 {
 		m.wuSid = ""
+		m.logger.ERROR.Println("Error while extracting WUI_SID. Body was= " + body)
 		return errors.New("Error while extracting WUI_SID. Body was= " + body)
 	}
 
 	m.wuSid = body[index+9 : index+9+15]
+
+	m.logger.DEBUG.Println("extractWuiSidFromBody: result=", m.wuSid)
 
 	return nil
 }
